be5c7aed0ebd665a6b4fb6dea075b17ad4a0719c
thomas-waite <thomas.waite4@gmail.com>
2021-03-16 15:46:03 +0000

    feat: tutorial 1

---
+++ Shared/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  cv-app
+//
+//  Created by Thomas Waite on 16/03/2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
