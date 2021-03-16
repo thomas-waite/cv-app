f3a645baaa330a718989577e1f8a5d6980246dfc
thomas-waite <thomas.waite4@gmail.com>
2021-03-16 13:36:27 +0000

    Initial Commit

---
+++ Shared/cv_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cv_appApp.swift
+//  Shared
+//
+//  Created by Thomas Waite on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct cv_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
